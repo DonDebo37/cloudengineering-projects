0c766f926066e2357c140ac2fa9619e2a9c9c0e5
DonDebo37 <adebolaadesanya54@gmail.com>
2024-11-13 19:03:42 +0000

    updated file

--- aa-blog/terraform-js/main.tf
+++ aa-blog/terraform-js/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block"
 resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 
     depends_on = [
-        aws_s3_bucket_ownership_controls.nextjs_bucket_ownership
+        aws_s3_bucket_ownership_controls.nextjs_bucket_ownership,
         aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
     ]
 
@@ -43,8 +43,8 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
     bucket = aws_s3_bucket.nextjs_bucket.id
     
-    policy = jsondecode {{{
-        version = "2012-10-17"
+    policy = jsonencode({
+        Version = "2012-10-17"
         Statement = [
             {
                 Sid = "PublicReadGetObject"
@@ -55,5 +55,57 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
             }
         ]
 
-    }}}
+    })
+}
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment = "Origin Access Identity for NextJS Portfolio site"
+}
+
+# Cloudfront Distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+
+    origin {
+        domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+        origin_id = "s3-nextjs-portfolio-bucket"
+
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }
+    }
+    enabled = true
+    is_ipv6_enabled = true
+    comment = "Cloudfront distribution for NextJS Portfolio site"
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "s3-nextjs-portfolio-bucket"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    restrictions {
+      geo_restriction {
+        restriction_type = "none"
+      }
+    }
+
+    viewer_certificate {
+
+        cloudfront_default_certificate = true
+
+    }
 }
--- aa-blog/terraform-js/state.tf
+++ aa-blog/terraform-js/state.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "aa-my-terraform-state"
+        bucket = "aa-my-tf-website-state"
         key = "global/s3/terraform.tfstate"
         region = "eu-west-2"
         dynamodb_table = "my-db-website-table"
